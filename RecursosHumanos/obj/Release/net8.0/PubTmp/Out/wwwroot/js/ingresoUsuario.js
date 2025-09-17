fd72b7cd77b1e2ac95f22c0d6b6669d76200232f
aleMontoya22 <vladioso2020@gmail.com>
2025-09-17 10:32:05 -0600

    Se configuro la cadena de conexion al archivo appsettings de produccion y al web.config

---
+++ RecursosHumanos/obj/Release/net8.0/PubTmp/Out/wwwroot/js/ingresoUsuario.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    $('#formIngresoUsuario').submit(function (e) {
+
+        e.preventDefault();
+
+        const email = $('#exampleInputEmail').val();
+        const password = $('#exampleInputPassword').val();
+
+        if (!email || !password) {
+            alert('Por favor completa los campos.');
+            return;
+        }
+
+        $.ajax({
+            url: urlGuardarUsuario,
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ Email: email, Contraseña: password }),
+            success: function (response) {
+                if (response.success) {
+                    window.location.href = urlLogin;
+                } else {
+                    alert(response.message || 'Credenciales inválidas');
+                }
+            },
+            error: function () {
+                alert('Error al intentar iniciar sesión.');
+            }
+        });
+    });
+});
